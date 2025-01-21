121bff814a3784611c7b800fcfba318a038f2cec
bocsir <jacobwharris13@gmail.com>
2025-01-21 16:20:13 -0500

    better split keyboard support

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "6","7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -96,10 +96,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_6,                      0)
+	TAGKEYS(                        XK_7,                      1)
+	TAGKEYS(                        XK_8,                      2)
+	TAGKEYS(                        XK_9,                      3)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
